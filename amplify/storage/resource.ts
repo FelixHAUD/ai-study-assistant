f4dc565ca217b7ae3736fa08b092a3970df8f388
johwuy <johnnyw8@uci.edu>
2025-05-10 13:25:40 -0700

    add read and write access to notes folder

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,9 @@ import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
   name: "amplifyTeamDrive",
+  access: (allow) => ({
+    "notes/*": [
+      allow.guest.to(["read", "write"]), // additional actions such as "write" and "delete" can be specified depending on your use case
+    ],
+  }),
 });
