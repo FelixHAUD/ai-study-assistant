6de174a074f7378e39b8bd337b9b583b376261df
johwuy <johnnyw8@uci.edu>
2025-05-11 12:25:41 -0700

    another potential fix for circular import

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,23 +3,19 @@ import { defineFunction, defineStorage } from "@aws-amplify/backend";
 export const storage = defineStorage({
   name: "amplifyTeamDrive",
   access: (allow) => ({
-    "notes/*": [
-      allow.guest.to(["read", "write", "delete"]),
-    ],
-    "transcriptions/*": [
-      allow.guest.to(["read", "write", "delete"]),
-    ],
-    "audio-recordings/*": [
-      allow.guest.to(["read", "write", "delete"])
-    ]
+    "notes/*": [allow.guest.to(["read", "write", "delete"])],
+    "transcriptions/*": [allow.guest.to(["read", "write", "delete"])],
+    "audio-recordings/*": [allow.guest.to(["read", "write", "delete"])],
   }),
   triggers: {
     onUpload: defineFunction({
       // relative to your backend/<category>/<resource>/src folder
       entry: "./on-upload-handler.ts",
+      resourceGroupName: "storage",
     }),
     onDelete: defineFunction({
       entry: "./on-delete-handler.ts",
+      resourceGroupName: "storage",
     }),
   },
 });
